c798adb8820911420ef05c7974ff27bab99631bc
jabberwalky <stas.tomich@gmail.com>
2014-03-05 22:46:42 +0400

    hw3(not complete)
    i’ll fix it on friday

---
+++ hw1/RouteViewController.h
@@ -0,0 +1,13 @@
+//
+//  RouteViewController.h
+//  hw1
+//
+//  Created by Admin on 05.03.14.
+//  Copyright (c) 2014 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RouteViewController : UITableViewController
+
+@end
